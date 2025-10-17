0ae9ad5d8b19eeab793c63cebd9b1a9042cb65a5
lopertut <artur.matvejev12345@gmail.com>
2025-10-17 21:19:54 +0300

    just a commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'product_list_angular';
+	products = Object.keys(localStorage);
+	localstorage = localStorage;
+	updated = false;
+
+	isUpdated() {
+		if (this.updated) {
+			this.products = Object.keys(localStorage);
+			this.updated = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	addProduct() {
+		const productName = (<HTMLInputElement>document.getElementById("name")).value;
+		const productPrice = (<HTMLInputElement>document.getElementById("price")).value;
+		localStorage.setItem(productName, productPrice);
+		this.updated = true;
+	}
+
+	removeProduct(product: string) {
+		this.localstorage.removeItem(product);
+		this.updated = true;
+	}
 }
